395aab4063005443581f9df85cd96bf0e3d9b962
JeudylRP <jeudylstudium@gmail.com>
2024-06-17 15:41:58 +0200

    xcx

---
+++ Python Challenge/Python Tutorial Deutsch (Komplettkurs) - Python lernen in unter 4 Stunden/pythonProject1/6dieInput()-Funktion.py
@@ -0,0 +1,9 @@
+#--- DIE INPUT()- Funktion
+
+print("Anfang des Programms")
+name = input ("Bitte deinen Namen eingeben: ")
+print("Mein name ist: " + name )
+age = input("Bitte deinen Alter eingeben: ")
+print("Mein Alter ist " + age)
+
+print("Ende des Programms")
