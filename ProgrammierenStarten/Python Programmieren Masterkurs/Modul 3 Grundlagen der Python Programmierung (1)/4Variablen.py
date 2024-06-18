1ea14d3e51370d3a3be43a76ba449d48203b371e
JeudylRP <jeudylstudium@gmail.com>
2024-06-18 09:22:06 +0200

    ccc

---
+++ ProgrammierenStarten/Python Programmieren Masterkurs/Modul 3 Grundlagen der Python Programmierung (1)/4Variablen.py
@@ -0,0 +1,9 @@
+'''
+------ VARIABLEN ------
+--- Variable benötigt eine wichtige Information: Bezeichner (Name der Variable)#
+--- Empfehlung: 
+- Variablen immer sprechen bennennen
+- Variablen im Idealfall mit englischen Bezeichner versehen.
+'''
+my_age = 26
+print(my_age)
