4ce7ba92a120f1f453f3b5b21a9b230f92e171ff
JeudylRP <jeudylstudium@gmail.com>
2024-08-15 17:14:35 +0200

    fgdgd

--- 1ProgrammierenStarten/Python Programmieren Masterkurs/Modul 11 Praxisprojekt - Space Invaders/FDasSpielFenstererzeugen.py
+++ 1ProgrammierenStarten/Python Programmieren Masterkurs/Modul 11 Praxisprojekt - Space Invaders/FDasSpielFenstererzeugen.py
@@ -2,22 +2,4 @@
 ------- 5. Das Spielfenster erzeugen:
 ------- 6. Titel, Framerate und Hintergrund modifizieren
 '''
-import pygame
-
-
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
-        self.running = True
-
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-
-if __name__ == "__main__":
-    game = Game()
-
-#343434343434DCDS
+print ("Hello ")
