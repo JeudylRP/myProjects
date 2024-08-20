bc67555b6c67615d13c0997492fbb48fa28a86e3
JeudylRP <jeudylstudium@gmail.com>
2024-08-20 18:51:06 +0200

    ggg

--- 1ProgrammierenStarten/Python Programmieren Masterkurs/Modul 11 Praxisprojekt - Space Invaders/FDasSpielFenstererzeugen.py
+++ 1ProgrammierenStarten/Python Programmieren Masterkurs/Modul 11 Praxisprojekt - Space Invaders/FDasSpielFenstererzeugen.py
@@ -8,11 +8,12 @@
 ------- 7. Das Raumschiff platzieren
 ------- 8. Bewegung eingrenzen
 ------- 8. Geschoss implementieren
-------- 10. ALiens Platzieren ---> 00:00
-------- 11.
+------- 10. ALiens Platzieren
+------- 11. Kollision zwischen Alien und Geschoss erfassen
 '''
 
 import pygame
+import random
 
 
 class Game:
@@ -25,7 +26,27 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
         self.spaceship = Spaceship(self, 370, 515)
-        self.background_img = pygame.image.load("spr_space_himmel.png")
+
+        self.enemies = []
+        for i in range(12):
+            self.enemies.append(Enemy(self, random.randint(0, 736), random.randint(30, 130))
+            '''
+            self.enemies.append(Enemy(self,random.randint(0,736), random.randint(0,130))
+            1. self.enemies.append(...)
+            - self.enemies: Das isi eine Liste, also wie eine Sammlung, in der wier Alle Aliens peichern.
+            - append(...): Das bedeutet "hinzufügen". Hier fügern wir ein neues Alien zu dieser Liste hinzu.
+            
+            2.Enemy(self, random.randint(0,736), random.randint(0,130)
+            
+            - Enemy(...): Hier wird ein neues Alien erstellt.
+            - self: Das bedeutet, dass dieses Alien zu dem Spiel gehört, das du geerade spielst.
+            
+            - random.rendint(0,736): Das wählt eine zufällige Zahl zwischen 0 und 736. Diese Zahl bestimmt, wie weit links oder rechts auf dem Bildschirm das Alien erscheinen Soll.
+            
+            - random.randint(0,130): Das wählt eine zufällige Zahl zwischen 0 und 130. Diese Zahl bestimmt, wie weit oben auf demm Bildschirm das Alien erscheinen soll.
+            ---  Zusammengefasst: Dieser Code erstellt ein Alien an einem zufälligeb Ort oben auf dem Bildschirm und fügt es zu Liste alles Alliens hinzu, die im Spiel existieren.
+            '''
+            self.background_img = pygame.image.load("spr_space_himmel.png")
         while self.running:
             self.clock.tick(60)
             self.screen.blit(self.background_img, (0, 0))
@@ -33,77 +54,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         self.spaceship.move(-10)
                     if event.key == pygame.K_RIGHT:
                         self.spaceship.move(10)
-                    if event.key == pygame.K_SPACE:
-                        self.spaceship.fire_bullet()
-
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                        self.spaceship.move(0)
-
-            self.spaceship.update()
-            if len(self.spaceship.bullets) > 0:
-                for bullet in self.spaceship.bullets:
-                    if bullet.is_fired:
-                        bullet.update()
-
-                    else:
-                        self.spaceship.bullets.remove(bullet)
-
-            pygame.display.update()
-
-class Spaceship:
-    def __init__(self, game,x,y):
-        self.x=x
-        self.y=y
-        self.change_x = 0
-        self.game = game
-        self.spaceship_img = pygame.image.load("spr_spaceship.png")
-        self.bullets = []
-    def fire_bullet(self):
-        self.bullets.append(Bullet(self.game, self.x, self.y))
-        self.bullets[len(self.bullets)-1].fire()
-
-    def move(self, speed):
-        self.change_x = speed
-    def update(self):
-        self.x += self.change_x
-        if self.x < 0:
-            self.x = 0
-        elif self.x > 736:
-            self.x = 736
-        self.game.screen.blit(self.spaceship_img, (self.x, self.y))
-
-class Bullet:
-     def __init__ (self,game,x,y):
-         self.x = x
-         self.y = y
-         self.game = game
-         self.is_fired = False
-         self.bullet_speed = 10
-         self.bullet_img = pygame.image.load("spr_patrone.png")
-
-     def fire(self):
-         self.is_fired = True
-     def update(self):
-         self.y -= self.bullet_speed #Geschoss nach oben bewegen
-         if self.y <= 0:
-             self.is_fired = False
-         self.game.screen.blit(self.bullet_img,(self.x, self.y))
-
-
-if __name__ == "__main__":
-    game = Game(800,600)
-    print(len(game.spaceship.bullets))
-
-
-
-
-
-
-
+                    if event.key == pygame.KE
